f7ccd37890af63ca3c18331dba06fd0838ddf307
talibkhan786 <101257948+talibkhan786@users.noreply.github.com>
2023-06-01 22:17:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import { useState } from 'react';
+import './App.css';
+import Homepage from './components/homepage/homepage'
+import Login from './components/login/login'
+import Register from './components/register/register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+
+  const [user, setLoginUser] = useState({
+
+  })
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path='/' element={
+            user && user._id ? < Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} />
+            } />
+          <Route path='/login' element={<Login setLoginUser={setLoginUser} />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+
+export default App;
